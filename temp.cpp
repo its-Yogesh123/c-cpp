31bd5ed0a2480c3f422bcf50c8bbd203e3bcabdd
Yogesh Kumar <yogesh.developer9@gmail.com>
2026-01-12 23:31:50 +0530

    Updating Bin

--- temp.cpp
+++ temp.cpp
@@ -1,18 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-
-class A{
-    int x(){
-        return 10;
-    }
-    public:
-    int x(){
-        return this->x();
-    }
-};
+int dp[31][31];
 int main(){
-    A obj;
-    cout<<obj.x();
+    for(int i=0;i<31;i++){
+        for(int j=0;j<31;j++){
+            if(i < j)dp[i][j] = 0;
+            else if(j==0)dp[i][j] = 1;
+            else dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+        }
+    }
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        int d=0;
+        while(n%2==0){
+            d++;
+            n/=2;
+        }
+        int ans=0;
+        for(int i=0;i<d;i++){
+            for(int j=1;j<=i+1;j++){
+                if(i+ j <= k)continue;
+                ans += dp[i][j-1];
+            }
+        }
+        if (d + 1 > k) ++ans;
+        cout<<ans<<endl;
+    }
+
     return 0;
 }
+
+
+
+
+
+*/
